6928abd4d641b6bdffc762599e70cfaa68725469
PriyankaSahu17 <73271026+PriyankaSahu17@users.noreply.github.com>
2024-07-12 16:39:38 +0530

    change commit

--- doctor-appointment/backend/routes/communications.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { getCommunicationHistory } from '../controllers/communicationController.js';
-
-const router = express.Router();
-
-router.get('/communications/:userId/:doctorId', getCommunicationHistory);
-
-export default router;
